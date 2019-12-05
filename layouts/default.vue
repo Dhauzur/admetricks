703c54ba2ffd0eba2c180e33081a7dba8fb86f85
Mauro Perez <mmauroperez@gmail.com>
2019-12-05 11:00:05 -0300

    Change directory

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nuxt class="nuxt" />
+  </div>
+</template>
+
+<style>
+body {
+  background-image: url("~assets/css/back1.png");
+  background-repeat: repeat;
+  background-size: 350px 300px;
+}
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
